9700df475e51a9e1ed0318330625e818f4204ced
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-25 15:38:59 -0500

    Add more Seed Data

--- models/inventoryStart.js
+++ models/inventoryStart.js
@@ -11,5 +11,19 @@ module.exports = [{
     qty: '10',
     img: 'https://i.imgur.com/MpyAMMs.jpg?1',
     desc: 'SPF protection of 15, lightwieght, water-wicking, and comes in multiple colors',
+},
+{
+    name: "Bottle",
+    price: "24.99",
+    qty: '10',
+    img: 'https://i.imgur.com/TVCVmO1.jpg',
+    desc: "Multiple Colors, Heavy Duty, Keeps drinks hot for hours and cold for days",
+},
+{
+    name: "Backpack",
+    price: "149.99",
+    qty: '3',
+    img: "https://i.imgur.com/HH90R05.jpg?1",
+    desc: "80lb backcountry backpack & has 3 color options (Red, Gray, Black)",
 }
 ]
